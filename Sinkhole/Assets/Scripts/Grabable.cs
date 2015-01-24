57e711d86893fd1ee65e530c9467bc4ecb1c2b06
Toni Pesola <pesola.toni@gmail.com>
2015-01-24 16:10:03 +0200

    Added logic to human link

---
+++ Sinkhole/Assets/Scripts/Grabable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabable : MonoBehaviour {
+	public bool grabable = true;
+	public Vector2 grabPoint = Vector2.zero;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
