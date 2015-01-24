6172c917fc3fbdc28911eb61731fe69174e0e937
Toni Pesola <pesola.toni@gmail.com>
2015-01-24 17:46:47 +0200

    Fixed some issues with link

--- Sinkhole/Assets/Scripts/GameState.cs
+++ Sinkhole/Assets/Scripts/GameState.cs
@@ -5,7 +5,7 @@ public class GameState : MonoBehaviour {
 	public Rigidbody2D linkEnd1;
 	public Rigidbody2D linkEnd2;
 	
-	private Rigidbody2D currentGrab;
+	public static Rigidbody2D currentGrab;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,16 +13,16 @@ public class GameState : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.Space) && currentGrab == null){
+		if(Input.GetKey(KeyCode.Space) && GameState.currentGrab == null){
 			grab();
 		}
-		else if(!Input.GetKey(KeyCode.Space) && currentGrab != null){
+		else if(!Input.GetKey(KeyCode.Space) && GameState.currentGrab != null){
 			releaseGrab();
 		}	
 	}
 	
 	void releaseGrab(){
-		currentGrab = null;
+		GameState.currentGrab = null;
 		linkEnd1.isKinematic = false;
 		linkEnd2.isKinematic = false;
 	}
@@ -30,11 +30,11 @@ public class GameState : MonoBehaviour {
 	void grab(){ // Grabs with higher link end
 			if(linkEnd1.transform.position.y > linkEnd2.transform.position.y){
 				linkEnd1.isKinematic = true;
-				currentGrab = linkEnd1;
+				GameState.currentGrab = linkEnd1;
 			}
 			else{
 				linkEnd2.isKinematic = true;
-				currentGrab = linkEnd2;
+				GameState.currentGrab = linkEnd2;
 			}
 		}
 }
---
+++ Sinkhole/Assets/Scripts/Swinging.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swinging : MonoBehaviour {
+
+	private float speed = 6f;
+	private bool incVelocity = true;
+
+	// Update is called once per frame
+	void Update () {
+		if(GameState.currentGrab != null){
+			float ix = Input.GetAxis("Horizontal");
+	
+			Vector3 vec = this.transform.position - GameState.currentGrab.transform.position;
+			float r = vec.magnitude;
+	
+			Vector3 tangent = (Vector3.Cross(vec, Vector3.back)).normalized;
+	
+			this.rigidbody2D.AddForce(new Vector2(tangent.x, tangent.y) * ix * speed * r * ((tangent.x > 0 ? tangent.x : 0)+ 0.2f));
+			incVelocity = true;
+		} else {
+			if(incVelocity){
+				this.rigidbody2D.velocity *= 1;
+				incVelocity = false;
+			}
+		}
+	}
+
+
+}
