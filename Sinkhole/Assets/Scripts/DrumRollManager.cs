87149fa0441b95712435336e446cf78bf69b65e6
Vornaskotti <janos@Puhuri.local>
2015-01-24 21:07:00 +0200

    Cowcrobat-GUI ja -animaatiot

---
+++ Sinkhole/Assets/Scripts/DrumRollManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrumRollManager : MonoBehaviour {
+
+	AudioSource drumRoll;
+
+	// Use this for initialization
+	void Awake () {
+		drumRoll = GetComponent <AudioSource> ();
+	}
+
+	void playDrumroll () {
+		Debug.Log ("drumroll");
+		drumRoll.Play ();
+	}
+
+}
