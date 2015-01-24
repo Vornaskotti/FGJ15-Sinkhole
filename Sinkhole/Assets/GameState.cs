7550f23ea52756236ba8bf913884935ac0a1cb1d
Toni Pesola <pesola.toni@gmail.com>
2015-01-24 12:27:01 +0200

    Added simple link grab

---
+++ Sinkhole/Assets/GameState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+	public Rigidbody2D linkEnd1;
+	public Rigidbody2D linkEnd2;
+	
+	private Rigidbody2D currentGrab;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space) && currentGrab == null){
+			grab();
+		}
+		else if(!Input.GetKey(KeyCode.Space) && currentGrab != null){
+			releaseGrab();
+		}	
+	}
+	
+	void releaseGrab(){
+		currentGrab = null;
+		linkEnd1.isKinematic = false;
+		linkEnd2.isKinematic = false;
+	}
+	
+	void grab(){ // Grabs with higher link end
+			if(linkEnd1.transform.position.y > linkEnd2.transform.position.y){
+				linkEnd1.isKinematic = true;
+				currentGrab = linkEnd1;
+			}
+			else{
+				linkEnd2.isKinematic = true;
+				currentGrab = linkEnd2;
+			}
+		}
+}
