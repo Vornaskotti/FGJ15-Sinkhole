f25a38046b0c5245d064180e583d35a6800a1056
mantoone <juha.eskonen@dnainternet.net>
2015-01-25 11:25:04 +0200

    Cow modified

--- Sinkhole/Assets/Scripts/Cow.cs
+++ Sinkhole/Assets/Scripts/Cow.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Cow : MonoBehaviour {
 	
 	public float range = 10f;
+	public Vector2 velocityRange = new Vector2(10f, 10f);
 	
 	// Use this for initialization
 	void Start () {
-		this.rigidbody2D.angularVelocity = Random.Range (-range, range);
-		Debug.Log(Random.Range (-range, range));
+		this.rigidbody2D.angularVelocity = Random.Range(-range, range);
 	}
 }
