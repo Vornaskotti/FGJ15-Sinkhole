aa856c2c60d712253807a92f028807c5e6741756
Juuso Toikka <jtoikka@gmail.com>
2015-01-25 13:53:37 +0200

    Cow stuff

---
+++ Sinkhole/Assets/Scripts/LavaForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+		rb.AddForce(new Vector2(0.0f, -8000.0f));
+	}
+}
